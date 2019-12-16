e1c50069fcf2538151868456111529a22f0936ac
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2019-12-16 13:21:42 +0300

    More comments

---
+++ README.md
@@ -0,0 +1,24 @@
+## Понедельник 16.12
+
+Скачайте скелет проекта и запустите его. Вам нужно запустить сервер -- `python3.7 server.py` --
+и браузер -- `http://localhost:5000`.
+
+Для ознакомления с JS сделайте упражнение 1 в `templates/index.html`: напишите выбор уникального имени пользователя. См. [Math.random()](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random).
+
+Далее можете ознакомиться с реализацией серверной части. Для этого попробуйте последовательно менять точку подключения браузера с `/ws1` до `/ws3` в index.html.
+
+Далее, сделайте упражнение 2: измените код хендлера `/ws3`, чтобы он распространял сообщения, полученные от одного пользователя (браузера) до всех остальных. Подсказка: заведите для этого множество очередей всех пользователей.
+
+Наконец, приступайте к упражнению 3: раскомментируйте в `templates/index.html` код кнопки счетчика -- и реализуйте механизм синхронизации!
+
+Подсказки:
+- при отправке структурированных данных используйте метод [JSON.stringify](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify) для превращения JS-объектов в JSON;
+- при получении используйте [JSON.parse](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse) для превращения JSON в JS-объект;
+- печатайте объекты в консоль браузера "как есть" через `console.log`;
+
+Полезные ссылки:
+* [Visual Studio Code](https://code.visualstudio.com/)
+* [Python 3.7+](https://www.python.org/downloads/windows/)
+* [Quart](https://pgjones.gitlab.io/quart/index.html) (`pip3 install -U quart`)
+* [Bootstrap](https://getbootstrap.com/)
+* [jQuery](https://api.jquery.com/)
--- server.py
+++ server.py
@@ -7,20 +7,28 @@ import quart
 
 app = quart.Quart(__name__, template_folder="templates", static_folder="static")
 log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format="> %(asctime)-15s %(levelname)-8s || %(message)s")
 
 @app.route("/")
 async def index():
     return await quart.render_template("index.html")
 
 
+# Шаг 1.
+# Хендл, который просто шлёт в ответ то же самое сообщение.
 @app.websocket("/ws1")
 async def ws1():
     while True:
         data = await quart.websocket.receive()
-        print(f"Got {data}!")
+        log.info(f"Получили от браузера %r", data)
         await quart.websocket.send(f"Подтверждаем, что получили '{data}'!")
 
 
+# Шаг 2.
+# Хендл, который шлёт раз в секунду текущее время сервера.
+# Важно, что в такой структуре кода развязаны циклы получения и отправки.
+# Это создает возможность отправлять сообщение от сервера к браузеру
+# не дожидаясь (как в шаге 1) сначала получения сообщения.
 @app.websocket("/ws2")
 async def ws2():
     user = quart.websocket.remote_addr
@@ -47,13 +55,16 @@ async def ws2():
         log.info("Пользователь %s отключился :(", user)
 
 
+# Шаг 3.
+# Связывем циклы получения и отправки сообщений через очередь.
+# Функционально то же самое, что и в ws1, но теперь мы можем в очередь на отправку
+# добавлять новые сообщения.
 @app.websocket("/ws3")
 async def ws3():
     user = quart.websocket.remote_addr
     queue = asyncio.Queue()
 
     async def sending():
-        import datetime
         while True:
             data = await queue.get()
             log.info("Пользователь %s: SEND[%r]", user, data)
@@ -74,10 +85,10 @@ async def ws3():
         log.info("Пользователь %s отключился :(", user)
 
 
+# Шаг 4.
 # TODO(2): Упражнение.
 # Реализуйте логику широковещательной отправки сообщений всем подключенным пользователям.
-# Для этого используйте множество очередей сообщений всех пользователей.
-
+# Подсказка: заведите множество очередей сообщений всех пользователей.
 
 def main():
     app.config.from_mapping(DEBUG=True, ENV="dev")
